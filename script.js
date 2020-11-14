b7b80b0505301e9547455711e37c7941df43fe38
karolklos <karolklos1986@gmail.com>
2020-11-14 10:32:11 +0100

    The Quiz Game

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ c) correct answer (I would use a number for this)
 
 
 (function () {
-  let counter = 0;
 
   function Question(question, answers, correct) {
     this.question = question;
@@ -40,13 +39,22 @@ c) correct answer (I would use a number for this)
     }
   }
 
-  Question.prototype.checkAnswer = function (ans) {
+  Question.prototype.checkAnswer = function (ans, callback) {
+    let sc;
+    
     if (ans === this.correct) {
-      counter++;
-      console.log('Correct answer! You have: ' + counter + ' points!');
+      console.log('Correct answer!');
+      sc = callback(true);
     } else {
       console.log('Wrong answer. Please try again');
+      sc = callback(false);
     }
+    this.displayScore(sc);
+  }
+
+  Question.prototype.displayScore = function(score) {
+    console.log('Your current sroce is ' + score);
+    console.log('------------------------------');
   }
 
   let q1 = new Question(
@@ -69,6 +77,18 @@ c) correct answer (I would use a number for this)
 
   let questions = [q1, q2, q3];
 
+  function score() {
+    let sc = 0;
+    return function(correct) {
+      if(correct) {
+        sc++;
+      }
+      return sc;
+    }
+  }
+
+  let keepScore = score();
+
   function nextQuestion() {
     let n = Math.floor(Math.random() * questions.length);
 
@@ -78,7 +98,7 @@ c) correct answer (I would use a number for this)
     const answer = prompt('Please select the correct answer.');
 
     if (answer !== 'exit') {
-      questions[n].checkAnswer(parseInt(answer));
+      questions[n].checkAnswer(parseInt(answer), keepScore);
       nextQuestion();
     }
   }
